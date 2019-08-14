61dbfff9f64cda6b4a0e2b2008016ae958e6201e
huheng <huheng1989@126.com>
2019-08-14 15:56:30 +0800

    add

---
+++ hls_download.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+m3u8_url=$1
+m3u8_dir=$2
+parallel=10
+
+if [ ! $m3u8_url ]; then
+    echo "usage ./m3u8_download.sh url dir "
+    exit 0
+fi
+
+if [ ! $m3u8_dir ]; then
+    m3u8_dir=xx
+fi
+
+echo "m3u8 url was: "${m3u8_url}
+mkdir -p ${m3u8_dir}
+cd ${m3u8_dir}
+
+echo "download m3u8..."
+wget ${m3u8_url}
+echo "download m3u8 done"
+
+# match 
+m3u8_base_url=${m3u8_url%/*}"/"
+m3u8_file_name=${m3u8_url##*/}
+echo "m3u8 http dir: ${m3u8_base_url}"
+echo "m3u8 file name: ${m3u8_file_name}"
+
+sub_dir=""
+
+n=1
+for line in `cat ${m3u8_file_name}`
+do
+if [ `echo ${line} | grep -c "\.ts$"` -eq '1' ]; then
+    echo "find ts url: ${line}"
+    if [ `echo ${line} | grep -c "http://"` -eq '1' ] || [ `echo ${line} | grep -c "https://"` -eq '1' ]; then
+        ts_uri=${line}
+    else
+        ts_uri=${m3u8_base_url}${line}
+        if [ -z ${sub_dir} ]; then
+            echo "mk subdir: "${sub_dir}
+            sub_dir=${line%/*}
+            mkdir -p $sub_dir
+            cd $sub_dir
+        fi
+    fi
+    
+    echo "ts url: ${ts_uri}"
+    array[$n]=$ts_uri
+    b=$(( $n % $parallel ))
+    if [ $b -eq 0 ];then
+        n=1
+        for url in ${array[@]}
+        do
+            wget $url &
+        done
+        wait $!
+    else
+        ((n+=1))
+    fi
+    #wget $ts_uri
+fi
+done
+
+for((i=1; i<$n; i++))
+do
+    wget -q ${array[i]}
+done
---
+++ readme.md
@@ -0,0 +1,3 @@
+## hls_download
+* use wget download m3u8 stream
+* download 10 segments concurrent
